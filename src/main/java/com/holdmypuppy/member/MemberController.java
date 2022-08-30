14888d5f1357ef4a5526a7f4c3827fa80c92888a
릴리\User <jon00141@naver.com>
2022-08-31 03:35:54 +0900

    lily-1104/HoldMyDog#6  - MemberController를 통해서 로그인 페이지 연결

---
+++ src/main/java/com/holdmypuppy/member/MemberController.java
@@ -0,0 +1,15 @@
+package com.holdmypuppy.member;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MemberController {
+	
+	// 로그인 페이지 연결
+	@GetMapping("/member/signin")
+	public String signinView() {
+		return "/member/signin";
+	}
+
+}
