3723b7be6939167434134d1aaef7de077a41630f
릴리\User <jon00141@naver.com>
2022-08-30 20:42:13 +0900

    (김지현)  - HoldMyPuppy (안아줄개) project 초기 설정

---
+++ src/main/java/com/holdmypuppy/hello/Hello.java
@@ -0,0 +1,16 @@
+package com.holdmypuppy.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Hello {
+	
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String helloWorld() {
+        return "Hello World!!";
+    }
+
+}
---
+++ src/test/java/com/holdmypuppy/HoldMyPuppyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.holdmypuppy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoldMyPuppyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
