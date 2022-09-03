42e97614636092475cf1e57f8e3b3ef39bf2c48c
릴리\User <jon00141@naver.com>
2022-09-03 13:38:36 +0900

    lily-1104/HoldMyPuppy#13, lily-1104/HoldMyPuppy#14  - 아이디 찾기, 비밀번호 찾기 JSP 연결!

--- src/main/java/com/holdmypuppy/member/MemberController.java
+++ src/main/java/com/holdmypuppy/member/MemberController.java
@@ -2,21 +2,41 @@ package com.holdmypuppy.member;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/member")
 @Controller
 public class MemberController {
 	
 	// 로그인 페이지 연결
-	@GetMapping("/member/signin")
+	@GetMapping("/signin")
 	public String signinView() {
 		return "/member/signin";
 	}
 	
 	
 	// 회원가입 페이지 연결
-	@GetMapping("/member/signup")
+	@GetMapping("/signup")
 	public String signupView() {
 		return "/member/signup";
 	}
+	
+	
+	
+	// 아이디 찾기 페이지 연결
+	@GetMapping("/find_id")
+	public String findIdView() {
+	    return "/member/findId";
+	}
+	
+	
+	
+	// 비밀번호 찾기 페이지 연결
+	@GetMapping("/find_pw")
+	public String findPwView() {
+	    return "/member/findPw";
+	}
+	
+	
 	
 }
