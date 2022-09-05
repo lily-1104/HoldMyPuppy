e05e212ce360c5f2ab1fd6a43c82d48123628521
릴리\User <jon00141@naver.com>
2022-09-06 02:27:20 +0900

    lily-1104/HoldMyPuppy#17  - 입양 후기 게시글 업로드를 위한 파일만 생성 (API)

---
+++ src/main/java/com/holdmypuppy/review/bo/ReviewBO.java
@@ -0,0 +1,8 @@
+package com.holdmypuppy.review.bo;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewBO {
+
+}
---
+++ src/main/java/com/holdmypuppy/review/dao/ReviewDAO.java
@@ -0,0 +1,8 @@
+package com.holdmypuppy.review.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewDAO {
+
+}
