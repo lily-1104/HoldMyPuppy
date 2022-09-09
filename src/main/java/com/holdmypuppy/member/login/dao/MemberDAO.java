853801102ba71c0e9bab9434d3a73172a1ba2439
릴리\User <jon00141@naver.com>
2022-09-09 23:42:53 +0900

    lily-1104/HoldMyPuppy#9, lily-1104/HoldMyPuppy#10, lily-1104/HoldMyPuppy#11, lily-1104/HoldMyPuppy#12, lily-1104/HoldMyPuppy#15  - 회원가입, 아이디 중복 확인, 닉네임 중복 확인, 로그인 API 경로 설정 변경

---
+++ src/main/java/com/holdmypuppy/member/login/dao/MemberDAO.java
@@ -0,0 +1,37 @@
+package com.holdmypuppy.member.login.dao;
+
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import com.holdmypuppy.member.login.model.Member;
+
+@Repository
+public interface MemberDAO {
+	
+	// 회원 가입
+	public int insertMember(
+			@Param("loginId") String loginId
+			, @Param("password") String password
+			, @Param("name") String name
+			, @Param("phoneNumber") String phoneNumber
+			, @Param("nickname") String nickname
+			, @Param("memberCode") char memberCode);
+	
+	
+	
+	// 회원 가입 - 아이디 중복 확인
+	public int selectCountLoginId(@Param("loginId") String loginId);
+	
+	
+	
+	// 회원 가입 - 닉네임 중복 확인
+	public int selectCountLoginNickname(@Param("nickname") String nickname);
+	
+	
+	
+	// 로그인 API
+	public Member selectMember(
+			@Param("loginId") String loginId
+			, @Param("password") String password);
+	
+}
