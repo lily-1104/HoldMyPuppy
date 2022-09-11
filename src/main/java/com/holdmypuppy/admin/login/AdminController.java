f771b456cc295cb5f5e89544b2437582dd03a386
릴리\User <jon00141@naver.com>
2022-09-11 23:50:19 +0900

    lily-1104/HoldMyPuppy#6  - 관리자 로그인, 회원가입, 로그아웃 페이지 package 추가

---
+++ src/main/java/com/holdmypuppy/admin/login/AdminController.java
@@ -0,0 +1,43 @@
+package com.holdmypuppy.admin.login;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+	
+	
+	// 관리자 회원가입 페이지 연결
+	@GetMapping("/signup")
+	public String adminSignup() {
+		return "/admin/signup_admin";
+	}
+	
+	
+	
+	// 관리자 로그인 페이지 연결
+	@GetMapping("/signin")
+	public String adminSignin() {
+		return "/admin/signin_admin";
+	}
+	
+	
+	
+	// 로그아웃
+	@GetMapping("/signout")
+	public String signOut(HttpServletRequest request) {
+		
+		HttpSession session = request.getSession();
+		session.removeAttribute("adminId");
+		session.removeAttribute("admminLoginId");
+		
+		return "redirect:/admin/signin";
+		
+	}
+	
+}
