19d2ed185f0e698b75533432247d20a8003462d9
릴리\User <jon00141@naver.com>
2022-09-14 03:35:02 +0900

    lily-1104/HoldMyPuppy#23  - 유기견 등록 API 구현 중 (미완)

---
+++ src/main/java/com/holdmypuppy/admin/inform/puppies/PuppiesRestController.java
@@ -0,0 +1,18 @@
+package com.holdmypuppy.admin.inform.puppies;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PuppiesRestController {
+	
+	
+	// 유기견 등록 API
+//	@PostMapping("/admin/puppies/post")
+//	public Map<String, String> postCreate(
+//			@RequestParam("title") String title
+//			, @RequestParam("dogName") String dogName
+//			, @RequestParam("breed") String breed
+//			, @RequestParam("age") int age
+//			, @RequestParam("gender") String gender)
+
+}
