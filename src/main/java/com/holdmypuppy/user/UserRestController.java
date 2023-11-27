4856b584a4088a5f158c32b2caf2419cd0967679
릴리\User <jon00141@naver.com>
2023-11-27 21:03:24 +0900

    lily-1104/HoldMyPuppy #1  - header 부분 로그인 시 상단에 닉네임 보이도록 설정 완료

--- src/main/java/com/holdmypuppy/user/UserRestController.java
+++ src/main/java/com/holdmypuppy/user/UserRestController.java
@@ -139,6 +139,7 @@ public class UserRestController {
 			session.setAttribute("userId", user.getId());
 			session.setAttribute("userLoginId", user.getLoginId());
 			session.setAttribute("userName", user.getName());
+			session.setAttribute("userNickname", user.getNickname());
 			
 			result.put("code", 200);
 			result.put("result", "성공");
